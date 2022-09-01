94c381ea155752092ebd7edd95b8efe13499fb40
khaledrefaat <khaledrefaat08@gmail.com>
2022-09-01 04:38:30 +0200

    solving till steps

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,36 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//     // // This is my solution
+//     return str.split(' ').map(str => str[0].toUpperCase() + str.slice(1)).join(' ')
+//
+//
+//     // // This is my solution
+//     // const strArray = str.split(' ');
+//     //
+//     // for (let index in strArray) {
+//     //     strArray[index] = strArray[index][0].toUpperCase() + strArray[index].slice(1);
+//     // }
+//     // return strArray.join(' ');
+// };
+
+
+// // This is stephen solution, and he is saying it's not the best way it's just another way of solving that problem
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
+
+console.log(capitalize('a short sentence'))
 
 module.exports = capitalize;
--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,55 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+// for (let i = 0; i < n; i++) {
+//     let str = '';
+//     for (let j = 0; j < n; j++) {
+//         if (j <= i) {
+//             str += '#';
+//         } else {
+//             str += ' ';
+//         }
+//     }
+//     console.log(str);
+// }
+// }
+
+////////////////////////////////////////////////////////////////////////////////
+// // My Recursive solution to the problem
+// function steps(n) {
+// function printHash(count, n, string = '') {
+//     if (n === 0) return console.log(string);
+//
+//     if (count > 0) {
+//         string += '#';
+//         count--;
+//         n--;
+//     } else {
+//         string += ' ';
+//         n--
+//     }
+//     printHash(count, n, string);
+// }
+//
+// for (let i = 1; i <= n; i++) {
+//     printHash(i, n);
+// }
+// }
+
+
+function steps(n, row = 0, stairs = '') {
+    if (n === row) return;
+    if (n === stairs.length) {
+        console.log(stairs);
+        return steps(n, row + 1);
+    }
+    if (row >= stairs.length) {
+        stairs += '#';
+    } else {
+        stairs += ' ';
+    }
+    steps(n, row, stairs);
+}
 
 module.exports = steps;
