731f4110dedccb1d4fb13a34dac9ffb16a0d35c6
khaledrefaat <khaledrefaat08@gmail.com>
2022-12-02 15:38:45 +0200

    solving weave

--- weave/index.js
+++ weave/index.js
@@ -24,6 +24,16 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+//  This is my Solution
+//  Actually stephen uses the same code to solve the problem
+function weave(sourceOne, sourceTwo) {
+  const q = new Queue();
+
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) q.add(sourceOne.remove());
+    if (sourceTwo.peek()) q.add(sourceTwo.remove());
+  }
+  return q;
+}
 
 module.exports = weave;
